5b06df1fc3afcb1fa9fcee727334668561cfec40
Mohammed Moharram <moharram82@hotmail.com>
2019-05-24 21:33:53 +0200

    first commit

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-05-24 17:09:53.
+    * Generated on 2019-05-24 19:33:21.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
